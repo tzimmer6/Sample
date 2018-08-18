a90ee50afa79374b14b23a844384911ff8abc082
tlzimmer <tlzimmer@ameritech.net>
2018-08-18 17:52:09 -0500

    This is committed from the Sample project

---
+++ src/us/tlzimmer/Pet.java
@@ -0,0 +1,17 @@
+package us.tlzimmer;
+
+/**
+ * Created by mgreen14 on 12/27/17.
+ */
+public abstract class Pet {
+    protected String name;
+    public Pet(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
---
+++ src/us/tlzimmer/Talkable.java
@@ -0,0 +1,9 @@
+package us.tlzimmer;
+
+/**
+ * Created by mgreen14 on 12/27/17.
+ */
+public interface Talkable {
+    String talk();
+    String getName();
+}

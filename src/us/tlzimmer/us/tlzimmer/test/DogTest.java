db2567f1e0dce71dc6d752adcefa77a1d962dda8
tlzimmer <tlzimmer@ameritech.net>
2018-08-21 17:26:32 -0500

    This is committed from the Sample project

---
+++ src/us/tlzimmer/us/tlzimmer/test/DogTest.java
@@ -0,0 +1,25 @@
+package us.tlzimmer.us.tlzimmer.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DogTest extends groovy.util.GroovyTestCase {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void isFriendly() {
+    }
+
+    @Test
+    public void talk() {
+    }
+
+    @Test
+    public void toString() {
+    }
+}

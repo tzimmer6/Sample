459784678c3d59e512f541f47cd06f22b4ec7a55
tlzimmer <tlzimmer@ameritech.net>
2018-08-22 05:41:59 -0500

    This is committed from the Sample project

---
+++ src/us/tlzimmer/test/DogTest.java
@@ -0,0 +1,44 @@
+package us.tlzimmer.test;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import us.tlzimmer.Dog;
+
+import static org.junit.Assert.*;
+
+public class DogTest extends TestCase {
+    Dog aDog1;
+    Dog aDog2;
+
+    public DogTest() {};
+
+
+    @Before
+    public void setUp() throws Exception {
+        aDog1 = new Dog(true, "Fido");
+        aDog2 = new Dog(false, "Spot");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testIsFriendly() {
+        assertTrue("The dog is friendly.", aDog1.isFriendly());
+        assertFalse("The dog is friendly.", aDog2.isFriendly());
+    }
+
+    @Test
+    public void testTalk() {
+        assertEquals("Bark", aDog1.talk());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("Dog: " + "name=" + aDog2.getName() + " friendly=" + aDog2.isFriendly(), aDog2.toString());
+    }
+
+}
